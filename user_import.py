2e2a1a5e9d7a40c77dcca24e4e04300e62ae5bbc
Chandan Kumar <chandank@chandank-lt.ma.net>
2013-04-06 19:27:32 -0700

    added shadow user import script

---
+++ user_import.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python -tt
+# This is a Free and Open Source Software
+# Copyright chandank@live.ca
+
+import sys
+import os
+
+user_file = 'user.list'
+
+# Read the file
+def read_user_file():
+    filp = open(user_file,"r")
+    user_lines = filp.readlines()
+    filp.close()
+    return user_lines
+
+#build the info list
+def build_info_list(user_lines):
+    user_info = []
+    for i in user_lines:
+        user_info.append(i.split())
+    return user_info
+
+# Create users and Change passwords
+def add_ipa_users(user_info):
+    count=0
+    for i in user_info:
+        count +=1
+        cmd="ipa user-add %s --first=%s --last=%s --uid=%s --setattr userPassword={crypt}%s" % (i[0],i[1],i[2],i[3],i[4])
+        print cmd
+        if count == 5:
+          break
+        #print "ipa user-add %s --first= --last= --uid= --setattr userPassword=" % (i)
+        #os.system(cmd)
+def main():
+    user_lines = read_user_file()
+    user_info = build_info_list(user_lines)
+    add_ipa_users(user_info)
+
+if __name__ == '__main__':
+    main()
+
