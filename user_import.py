9026dcfc6c2ba641a6f0f84dcb5b91b2143f0d46
chandan <chandank@med-access.net>
2013-04-16 15:20:19 -0700

    added shadow and passwd file support

--- user_import.py
+++ user_import.py
@@ -1,41 +1,57 @@
 #!/usr/bin/python -tt
 # This is a Free and Open Source Software
-# Copyright chandank.kumar@gmail.com
 
 import sys
 import os
 
-user_file = 'user.list'
-
-# Read the file
-def read_user_file():
-    filp = open(user_file,"r")
-    user_lines = filp.readlines()
-    filp.close()
-    return user_lines
+passwd_file = 'passwd'
+shadow_file = 'shadow'
+user_info = []
 
 #build the info list
-def build_info_list(user_lines):
-    user_info = []
-    for i in user_lines:
-        user_info.append(i.split())
+def build_info_list():
+
+    global passwd_file
+    global shadow_file
+    global user_info
+
+    filp = open(passwd_file,"r")
+    passwd_lines = filp.readlines()
+    filp.close()
+    filp = open(shadow_file, "r")
+    shadow_lines = filp.readlines()
+    filp.close()
+
+    count=0
+    for i,j in zip(passwd_lines,shadow_lines):
+        pt = j.split(':')
+        ut = i.split(':')
+        user_info.append((ut[0],ut[2],ut[4],pt[1]))
+        if count==58:
+          break
+        count +=1
     return user_info
 
+
 # Create users and Change passwords
-def add_ipa_users(user_info):
-    count=0
+def add_ipa_users():
+
+    global user_info
+    cmd = 'ipa config-mod --enable-migration=true'
+    os.system(cmd)
+
     for i in user_info:
-        count +=1
-        cmd="ipa user-add %s --first=%s --last=%s --uid=%s --setattr userPassword=%s" % (i[0],i[1],i[2],i[3],i[4])
-        print cmd
-        if count == 5:
-          break
-        #print "ipa user-add %s --first= --last= --uid= --setattr userPassword=" % (i)
-        #os.system(cmd)
+        cmd="ipa user-add %s --first=%s --last=%s --uid=%s --email=%s --setattr userPassword='{CRYPT}%s'" % (i[0],i[0],i[0],i[1],i[2],i[3])
+    #    print cmd
+        os.system(cmd)
+
+    cmd = 'ipa config-mod --enable-migration=false'
+    os.system(cmd)
+
 def main():
-    user_lines = read_user_file()
-    user_info = build_info_list(user_lines)
-    add_ipa_users(user_info)
+    build_info_list()
+    add_ipa_users()
 
 if __name__ == '__main__':
     main()
+
