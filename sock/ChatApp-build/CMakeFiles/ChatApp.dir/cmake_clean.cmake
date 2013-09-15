f9b0b84711abaa4bff7fb83cdd4749bf6bafeabe
internet <internet@localhost.localdomain>
2013-09-15 00:19:25 -0700

    added cmake chatapp dirs

---
+++ sock/ChatApp-build/CMakeFiles/ChatApp.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/ChatApp.dir/main.cpp.o"
+  "ChatApp.pdb"
+  "ChatApp"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/ChatApp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
